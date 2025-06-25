1ebd3b1f45b8b455d2f7edd6ef32bc6c805eecaa
Giulia Fanelli <gifanell@student.42firenze.it>
2025-06-25 22:57:40 +0200

    norminettato

--- push_swap.h
+++ push_swap.h
@@ -6,12 +6,12 @@
 /*   By: gifanell <gifanell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 16:36:26 by gifanell          #+#    #+#             */
-/*   Updated: 2025/06/25 22:27:29 by gifanell         ###   ########.fr       */
+/*   Updated: 2025/06/25 22:56:31 by gifanell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
-#define PUSH_SWAP_H
+# define PUSH_SWAP_H
 
 # include "libft.h"
 
@@ -21,15 +21,16 @@
 
 typedef struct s_node
 {
-	int				value;	// valore del nodo
+	int				value;
 	int				total_cost;
-	
+
 	int				rev_rot_a;
 	int				rev_rot_b;
 	int				rot_a;
 	int				rot_b;
-	struct s_node	*next; // puntatore al nodo successivo
+	struct s_node	*next;
 }	t_node;
+
 
 // Prototipi delle funzioni
 t_node	*stack_new(int value); // crea un nuovo nodo per la pila
@@ -38,12 +39,12 @@ int		stack_size(t_node *stack); // conta il numero di nodi nella pila
 void	free_stack(t_node **stack); // libera la memoria della pila
 int		is_sorted(t_node *stack); // controlla se la pila è gia' ordinata
 int		is_duplicate(t_node *stack, int value); // verifica se un valore è già presente nella pila
-void    error_exit(t_node **stack); // gestisce gli errori
+void	error_exit(t_node **stack); // gestisce gli errori
 bool	is_integer(const char *str); // verifica se una stringa rappresenta un intero
 void	parse_args(t_node **stack, char **argv); // analizza gli argomenti e popola la pila
 
 // Funzioni per operazioni/gestione della pila
-void    push(t_node **src, t_node **dest, int should_print, char *str); // sposta il primo elemento da stack_a a stack_b
+void	push(t_node **src, t_node **dest, int should_print, char *str); // sposta il primo elemento da stack_a a stack_b
 void	push_a(t_node **stack_a, t_node **stack_b);
 void	push_b(t_node **stack_a, t_node **stack_b);
 void	rotate(t_node **stack, int should_print, char *str); // ruota gli elementi della pila verso l'alto
@@ -56,7 +57,7 @@ void	swap(t_node **stack, int should_print, char *str); // scambia i primi due e
 void	swap_a(t_node **stack);
 void	rotate_both(t_node **stack_a, t_node **stack_b); // ruota entrambe le pile verso l'alto
 void	reverse_rotate_both(t_node **stack_a, t_node **stack_b); // ruota entrambe le pile verso il basso
-void    swap_both(t_node **stack_a, t_node **stack_b); // scambia i primi due elementi di entrambe le pile
+void	swap_both(t_node **stack_a, t_node **stack_b); // scambia i primi due elementi di entrambe le pile
 
 t_node	*find_min(t_node *stack); // trova il nodo con il valore minimo nella pila
 int		find_max(t_node *stack); // trova il valore massimo nella pila
